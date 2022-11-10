c650a9a44031b718e7163adf700c71b9b7ef48ff
= <tkng2001@gmail.com>
2022-11-10 21:17:19 +0700

    cod1

---
+++ Cluster.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KmeanPic
+{
+    class Cluster
+    {
+        private string name;
+        private Vector vectorOC;
+        private List<Picture> picturesListOC;
+
+        public Cluster(Vector vectorOC, List<Picture> picturesListOC1)
+        {
+            VectorOC = vectorOC;
+            PicturesListOC = picturesListOC1;
+        }
+        public Cluster(Vector vectorOC)
+        {
+            VectorOC = vectorOC;
+            PicturesListOC = new List<Picture>();
+        }
+
+        internal Vector VectorOC { get => vectorOC; set => vectorOC = value; }
+        internal List<Picture> PicturesListOC { get => picturesListOC; set => picturesListOC = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
