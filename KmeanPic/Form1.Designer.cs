970ebcd30bc2908caa09ffabee8cb8492de2d575
= <tkng2001@gmail.com>
2022-11-10 21:34:07 +0700

    fullUpdate

---
+++ KmeanPic/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace KmeanPic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl3 = new DevExpress.XtraEditors.PanelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.btnSearch = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.xtraOpenFileDialog1 = new DevExpress.XtraEditors.XtraOpenFileDialog(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).BeginInit();
+            this.panelControl3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            this.panelControl2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.panelControl3);
+            this.panelControl1.Controls.Add(this.panelControl2);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(575, 343);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // panelControl3
+            // 
+            this.panelControl3.Controls.Add(this.labelControl4);
+            this.panelControl3.Controls.Add(this.labelControl3);
+            this.panelControl3.Controls.Add(this.labelControl2);
+            this.panelControl3.Controls.Add(this.btnSearch);
+            this.panelControl3.Controls.Add(this.labelControl1);
+            this.panelControl3.Controls.Add(this.textEdit1);
+            this.panelControl3.Controls.Add(this.simpleButton1);
+            this.panelControl3.Controls.Add(this.pictureEdit1);
+            this.panelControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl3.Location = new System.Drawing.Point(2, 2);
+            this.panelControl3.Name = "panelControl3";
+            this.panelControl3.Size = new System.Drawing.Size(402, 339);
+            this.panelControl3.TabIndex = 1;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(49, 303);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(100, 13);
+            this.labelControl4.TabIndex = 7;
+            this.labelControl4.Text = "Thời gian truy vấn   :";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(49, 271);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(99, 13);
+            this.labelControl3.TabIndex = 6;
+            this.labelControl3.Text = "Độ đa dụng hóa      :";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(49, 240);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(99, 13);
+            this.labelControl2.TabIndex = 5;
+            this.labelControl2.Text = "Độ phủ                    :";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Location = new System.Drawing.Point(293, 167);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(74, 23);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(49, 209);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(99, 13);
+            this.labelControl1.TabIndex = 3;
+            this.labelControl1.Text = "Độ chính xác           :";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(49, 167);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.AutoHeight = false;
+            this.textEdit1.Size = new System.Drawing.Size(120, 23);
+            this.textEdit1.TabIndex = 2;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(164, 167);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 1;
+            this.simpleButton1.Text = "Chọn ảnh";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // pictureEdit1
+            // 
+            this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
+            this.pictureEdit1.Location = new System.Drawing.Point(87, 10);
+            this.pictureEdit1.Name = "pictureEdit1";
+            this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
+            this.pictureEdit1.Properties.SvgImageSize = new System.Drawing.Size(140, 140);
+            this.pictureEdit1.Size = new System.Drawing.Size(244, 142);
+            this.pictureEdit1.TabIndex = 0;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Controls.Add(this.listView1);
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelControl2.Location = new System.Drawing.Point(404, 2);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(169, 339);
+            this.panelControl2.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(2, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(165, 335);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // xtraOpenFileDialog1
+            // 
+            this.xtraOpenFileDialog1.FileName = "xtraOpenFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 343);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "Form1";
+            this.Text = "Tìm kiếm ảnh";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).EndInit();
+            this.panelControl3.ResumeLayout(false);
+            this.panelControl3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            this.panelControl2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.PanelControl panelControl3;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private DevExpress.XtraEditors.PictureEdit pictureEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.SimpleButton btnSearch;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.XtraOpenFileDialog xtraOpenFileDialog1;
+    }
+}
+
---
+++ KmeanPic/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KmeanPic
+{
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        //Image list 
+        List<Picture> PictureList = new List<Picture>();
+        List<Cluster> ClusterList = new List<Cluster>();
+        string fileName = Path.Combine(Environment.CurrentDirectory, @"..\..\Resources\", "FeatureVectors.txt");
+        string folder = Path.Combine(Environment.CurrentDirectory, @"..\..\Resources\");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                System.IO.DirectoryInfo di = new DirectoryInfo(folder + "/Cluster/");
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+                if (!Directory.EnumerateFileSystemEntries(folder + "/Cluster/").Any())
+                {
+                    VectorHandle.input(fileName, PictureList);
+                    ClusterList = Kmean.kmean(PictureList);
+                    VectorHandle.output(ClusterList);
+                }    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: Can't read file" + ex.Message, "Notify", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            //listView1.Items.Clear();
+            //listView1.View = View.SmallIcon;
+            ////tạo danh sách ảnh
+            //ImageList listImg = new ImageList() { ImageSize = new Size(70, 70) };
+            //listView1.SmallImageList = listImg;
+            ////gán cho listView(lst)
+            //int k = 0;
+            //for (int i = 100; i < 110; i++)
+            //{
+            //    try
+            //    {
+            //        listImg.Images.Add(System.Drawing.Image.FromFile("../../Image/beach/" + i + ".jpg"));
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        continue;
+            //    }
+            //    ListViewItem item = new ListViewItem() { Text = i + ".jpg" };
+            //    item.ImageIndex = k++;
+            //    listView1.Items.Add(item);
+            //}
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (xtraOpenFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureEdit1.Image = System.Drawing.Image.FromFile(xtraOpenFileDialog1.FileName);
+                textEdit1.Text = xtraOpenFileDialog1.SafeFileName;
+                btnSearch.Enabled = true;
+            }
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+                search(xtraOpenFileDialog1.SafeFileName, listView1);
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Error:" + ex.Message, "Notify", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            //}
+        }
+        public void search(string fileName, ListView lst)
+        {
+            //tìm ảnh có fileName truyền vào từ danh sách listItem
+            Picture pic = PictureList.Find(picture => picture.ImgName.Equals(fileName));
+            //tính khoảng cách từ ảnh đó đến từng cụm coi gần cụm nào nhất (khoảng cách min)
+            double minDistance = double.MaxValue;
+            string searchName = "";
+            foreach (var cluster in ClusterList)
+            {
+                double distance = VectorHandle.Distance(cluster.VectorOC, pic.Vector);
+                if (minDistance > distance)
+                {
+                    minDistance = distance;
+                    searchName = cluster.Name;
+                }
+            }
+            //khai báo danh sách những ảnh tương tự
+            List<Picture> listSearch = new List<Picture>();
+            //vào file có tên listCum[vt].TenThuMuc đọc ra
+            var pictures = File.ReadAllLines(folder + "/Cluster/" + searchName + ".txt");
+            //phần này giống như hàm đọc file trên (input())
+            foreach (var picture in pictures)
+            {
+                pic = new Picture();
+                Vector vector = new Vector();
+                var str = picture.Trim().Split(' ', '(', ')');
+                pic.Id = str[0];
+                for (int i = 1; i < str.Length - 2; i++)
+                {
+                    if (str[i].Contains(".jpg"))
+                        pic.ImgName = str[i];
+                    else
+                        try
+                        {
+                            vector.Properties.Add(Double.Parse(str[i]));
+                        }
+                        catch (Exception ex)
+                        {
+                            continue;
+                        }
+                }
+                pic.Vector = vector;
+                pic.FolderName = str[str.Length - 2];
+                listSearch.Add(pic);
+            }
+            lst.Items.Clear();
+            lst.View = View.SmallIcon;
+            //tạo danh sách ảnh
+            ImageList listImg = new ImageList() { ImageSize = new Size(70, 70) };
+            //gán cho listView(lst)
+            lst.SmallImageList = listImg;
+            int k = 0;
+            foreach (Picture image in listSearch)
+            {
+                try
+                {
+                    listImg.Images.Add(Image.FromFile(folder + "/Image/" + image.FolderName + "/" + image.ImgName));
+                }
+                catch (Exception ex)
+                {
+                    continue;
+                }
+                ListViewItem item = new ListViewItem() { Text = image.ImgName };
+                item.ImageIndex = k++;
+                lst.Items.Add(item);
+            }
+        }
+    }
+}
